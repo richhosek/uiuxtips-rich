6adff09b2eebe97a43f796bf73d40acac199889c
Rich Hosek <richhosek@gmail.com>
2023-12-12 22:03:57 -0600

    first commit

---
+++ routes/diagnostics.js
@@ -0,0 +1,37 @@
+const diagnostics = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+// GET Route for retrieving diagnostic information
+diagnostics.get('/', (req, res) => {
+    // TODO: Logic for sending all the content of db/diagnostics.json
+    readFromFile("./db/diagnostics.json")
+        .then(data => res.json(JSON.parse(data)));
+});
+
+// POST Route for a error logging
+diagnostics.post('/', (req, res) => {
+    // TODO: Logic for appending data to the db/diagnostics.json file
+    console.log(req.body)
+    const { username, topic, tip } = req.body;
+
+    if (req.body) {
+        const diagnostic = {
+            errors: {
+                username,
+                tip,
+                topic
+            },
+            error_id: uuidv4(),
+            time: Date.now() 
+        };
+
+        readAndAppend(diagnostic, './db/diagnostics.json');
+        console.log(diagnostic)
+        res.json(`Diagnostic added successfully`);
+    } else {
+        res.error('Error in adding diagnostic');
+    }
+});
+
+module.exports = diagnostics;
